9a15b208a53cc56837efb01f13d6c7052c77b33c
zxorical <newestguyemailfr@gmail.com>
2026-07-27 13:08:51 -0300

    Document autoJoin module and adjust exports
    Added module documentation and updated exports.

--- src/autoJoin/index.ts
+++ src/autoJoin/index.ts
@@ -1,8 +1,19 @@
+/**
+ * @module autoJoin
+ * 
+ * AutoJoin module exports
+ */
+
+// Main class
 export { AutoJoinManager } from './manager.js';
+
+// Types
 export type { 
   GiveawayEntry, 
-  GiveawayButton, 
+  GiveawayButton,
   GiveawayStats,
   EntryStatus 
 } from './manager.js';
-export { globalQueue } from './manager.js';
+
+// If you need globalQueue, uncomment this AFTER adding export to manager.ts
+// export { globalQueue } from './manager.js';
