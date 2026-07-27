186432fb8433124acddc8e61a8faebe23fbfb051
zxorical <newestguyemailfr@gmail.com>
2026-07-27 13:09:53 -0300

    Update index.ts

--- src/autoJoin/index.ts
+++ src/autoJoin/index.ts
@@ -1,19 +1,1 @@
-/**
- * @module autoJoin
- * 
- * AutoJoin module exports
- */
-
-// Main class
 export { AutoJoinManager } from './manager.js';
-
-// Types
-export type { 
-  GiveawayEntry, 
-  GiveawayButton,
-  GiveawayStats,
-  EntryStatus 
-} from './manager.js';
-
-// If you need globalQueue, uncomment this AFTER adding export to manager.ts
-// export { globalQueue } from './manager.js';
