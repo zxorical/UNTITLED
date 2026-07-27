1485a397bba3a03058a2d787f85ad9b58acdfb18
zxorical <newestguyemailfr@gmail.com>
2026-07-27 12:47:44 -0300

    Update index.ts

--- src/autoJoin/index.ts
+++ src/autoJoin/index.ts
@@ -1,1 +1,8 @@
 export { AutoJoinManager } from './manager.js';
+export type { 
+  GiveawayEntry, 
+  GiveawayButton, 
+  GiveawayStats,
+  EntryStatus 
+} from './manager.js';
+export { globalQueue } from './manager.js';
